540d48fb945901728a6810a8ac0c059e87adb695
mmc6-create <mmcmanu6@kent.edu>
2026-05-06 12:17:39 -0400

    So many rooms

---
+++ MoonlightMansion_p4/assets/backgrounds/4TileSet1 (4).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="4TileSet1 (4)" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="../../../../../../Downloads/4TileSet1 (4).png" trans="08f700" width="256" height="256"/>
+</tileset>
